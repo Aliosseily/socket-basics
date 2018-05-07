49fecbbb8e4e528a035b5096822a6e0316009fc4
Aliosseily <ali.ossaily@outlook.com>
2018-05-07 16:48:23 +0300

    second part

--- back/index.js
+++ back/index.js
@@ -6,6 +6,10 @@ const handleRequest = (req, res) => {
   res.end('ok!')
 }
 
+const socket = require('socket.io');
+
+const catNames = require('cat-names');
+
 server.on('request', handleRequest)
 server.listen(8888, ()=> console.log(`server is ready`))
 
@@ -15,7 +19,11 @@ let globalNumber = 0
 
 io.on('connection', (socket) => {
   
+  const username = catNames.random()
+
   console.log('a user connected')
+  io.emit('user:new',username)
+  socket.emit('user:me',username) 
 	
   socket.on('disconnect', () => {
     console.log('user disconnected');
@@ -32,5 +40,9 @@ io.on('connection', (socket) => {
   });
 
   socket.emit('number:change',globalNumber)
+
+  socket.on('SEND_MESSAGE', function(data){
+    io.emit('RECEIVE_MESSAGE', data);
+})
 
 });
--- front/src/App.js
+++ front/src/App.js
@@ -4,15 +4,50 @@ import io from 'socket.io-client'
 
 class App extends Component{
   
-    state = { socket:null, globalNumber:0 }
+   state = 
+    { 
+    socket:null,
+    globalNumber:0 ,
+    username: '',
+    message: '',
+    messages: [] 
+  }
   
     componentDidMount(){
       const socket = io('http://localhost:8888');
+
+      socket.on('RECEIVE_MESSAGE', function(data){
+        addMessage(data);
+    });
+
+    const addMessage = data => {
+        console.log(data);
+        this.setState({messages: [...this.state.messages, data]});
+        console.log(this.state.messages);
+    };
+
+    this.sendMessage = ev => {
+        ev.preventDefault();
+        socket.emit('SEND_MESSAGE', {
+            user: this.state.username,
+            message: this.state.message
+        })
+        this.setState({message: ''});
+
+    }
   
-      this.setState({socket})
+      this.setState({socket}) 
   
       socket.on('number:change', (globalNumber) => {
     this.setState({globalNumber})
+      })
+
+      socket.on('user:new', (username)=>{
+        console.log('a user called '+username+' has connected')     
+       })
+
+       socket.on('user:me', (username)=>{
+        this.setState({username})
       })
   
     }
@@ -21,16 +56,36 @@ class App extends Component{
     onDecrement = () => this.state.socket.emit('decrement')
     render(){
       return (
-        <div className="App">
+        <div>
          
     <h1>{this.state.globalNumber}</h1>
-          <p className="App-intro">
+          <p>
             <button onClick={this.onIncrement}>+</button>
             <button onClick={this.onDecrement}>-</button>
+            {this.state.username+' has connected'}
+            {this.sendMessage}
           </p>
+          <div>
+          <div className="messages">
+
+                {this.state.messages.map(message => {
+                    return (
+                             <div>{message.user}: {message.message}</div>
+                           )
+                    })}
+          </div>
+          <input type="text" placeholder="Username" className="username" value={this.state.username} />
+          <br/>
+          <input type="text" placeholder="Message" className="message" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+          <br/>
+          <button onClick={this.sendMessage}  className="Sendbtn">Send</button>
+          </div>
         </div>
       );
     }
   }
   
   export default App;
+
+
+  
